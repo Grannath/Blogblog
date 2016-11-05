126fa7ad1f14f35dd6e0450a4202bd7aa184d784
Grannath <johannes.zick@posteo.de>
2016-11-05 18:16:23 +0100

    Creates first init migration.
    A first migration creates the app user, if it does not exist. It also creates tables for blog users and posts. The jOOQ classes generated from this are added as well.

---
+++ src/main/resources/db/migration/V1_0_0__Initialize_DB.sql
@@ -0,0 +1,36 @@
+DO
+$body$
+BEGIN
+  IF NOT EXISTS(
+      SELECT *
+      FROM pg_catalog.pg_user
+      WHERE usename = 'blog_user')
+  THEN
+    CREATE ROLE blog_user LOGIN PASSWORD 'blogblog';
+  END IF;
+END
+$body$;
+
+CREATE TABLE blogblog.bl_users (
+  id          SERIAL,
+  username    VARCHAR(50)           NOT NULL UNIQUE,
+  password    VARCHAR(50)           NOT NULL,
+  deactivated BOOLEAN DEFAULT FALSE NOT NULL,
+  PRIMARY KEY (id)
+);
+
+GRANT SELECT, INSERT ON blogblog.bl_users TO blog_user;
+GRANT UPDATE (username, deactivated) ON blogblog.bl_users TO blog_user;
+
+CREATE TABLE blogblog.bl_posts (
+  id      SERIAL,
+  title   TEXT                                NOT NULL,
+  content TEXT                                NOT NULL,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  author  INTEGER                             NOT NULL REFERENCES blogblog.bl_users,
+  hidden  BOOLEAN DEFAULT FALSE               NOT NULL,
+  PRIMARY KEY (id)
+);
+
+GRANT SELECT, INSERT ON blogblog.bl_posts TO blog_user;
+GRANT UPDATE (hidden) ON blogblog.bl_posts TO blog_user;
